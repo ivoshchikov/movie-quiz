e4e79901b83cab57ef04a74ea9ee44958951f4bd
i.voshchikov <voschikov_ivan@mail.ru>
2025-06-14 12:31:33 +0300

    Добавить vite.config.js для сборки фронтенда

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+// файл: movie-quiz/frontend/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/",           // чтобы все пути к ассетам были абсолютными от корня
+  plugins: [react()],
+  build: {
+    outDir: "../backend/static",  // ← сборка будет прямо в backend/static
+    emptyOutDir: true,            // содержимое папки backend/static перед билдом очищается
+    assetsDir: "assets",          // js/css попадут в backend/static/assets
+  },
+  server: {
+    proxy: {
+      "/question": "http://localhost:8000",
+      "/answer":   "http://localhost:8000",
+    },
+  },
+});
