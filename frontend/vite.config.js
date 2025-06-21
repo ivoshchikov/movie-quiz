38a20521d036460585960c32b1babe8c7ba1fb88
i.voshchikov <voschikov_ivan@mail.ru>
2025-06-21 03:05:20 +0300

    build: enable sourcemaps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
-// файл: movie-quiz/frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",           // чтобы все пути к ассетам были абсолютными от корня
+  base: "/",
   plugins: [react()],
   build: {
-    outDir: "../frontend/dist",  // ← сборка будет прямо в backend/static
-    emptyOutDir: true,            
+    outDir: "../frontend/dist",
+    emptyOutDir: true,
+    sourcemap: true       // ← добавьте
   },
   server: {
     proxy: {
       "/question": "http://localhost:8000",
-      "/answer":   "http://localhost:8000",
+      "/answer": "http://localhost:8000",
     },
   },
 });
